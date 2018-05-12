d8dd8a54055d0ae10faad678cf50d15cffb6fd59
Joan Carles Jimenez <jjcarles@jocajica.com>
2018-05-12 19:03:32 +0200

    First commit

---
+++ app/src/main/java/com/jocajica/project_004/activities/MainActivity.java
@@ -0,0 +1,205 @@
+package com.jocajica.project_004.activities;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.jocajica.project_004.R;
+import com.jocajica.project_004.fragments.MainFragment;
+import com.jocajica.project_004.fragments.SettingsFragment;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements MainFragment.OnMainListener {
+
+    private int mPosStart;
+    private int mPosEnd;
+
+    private boolean mIsStartActive;
+    private boolean mIsRunning;
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private Set<BluetoothDevice> mPairedDevices;
+
+    private Fragment mCurrentFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+        init();
+
+        //initBT();
+
+        mCurrentFragment = new MainFragment();
+        changeFragment(mCurrentFragment);
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.containerFragment, fragment);
+        transaction.commit();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        }
+    }
+
+    private void initBT() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Dispositivo no compatible", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void init() {
+        mPosStart = 0;
+        mPosEnd = 0;
+        mIsStartActive = true;
+        mIsRunning = false;
+    }
+
+    private void takePhoto() {
+
+    }
+
+    private void startEndSession() {
+        mIsRunning = !mIsRunning;
+
+        ((MainFragment)mCurrentFragment).setStartEndStatus(mIsRunning);
+
+        if (mIsRunning) {
+            sendDataToBt("START;");
+        } else {
+            sendDataToBt("FINISH;");
+        }
+    }
+
+    private void sendDataToBt(String command) {
+
+    }
+
+    private void configure() {
+        mCurrentFragment = new SettingsFragment();
+        changeFragment(mCurrentFragment);
+    }
+
+    private void setPosition() {
+        mIsStartActive = !mIsStartActive;
+
+        ((MainFragment) mCurrentFragment).setRunStopStatus(mIsStartActive);
+
+        if (mIsStartActive) {
+
+        } else {
+
+        }
+    }
+
+    private void moveForward() {
+        if (mIsStartActive) {
+            mPosStart++;
+            mPosEnd = mPosStart;
+        } else {
+            mPosEnd++;
+        }
+
+        ((MainFragment)mCurrentFragment).setInterval(mPosStart, mPosEnd);
+    }
+
+    private void moveBackward() {
+        if (mIsStartActive) {
+            mPosStart--;
+            mPosEnd = mPosStart;
+        } else {
+            if (mPosEnd > mPosStart) {
+                mPosEnd--;
+            }
+        }
+
+        ((MainFragment)mCurrentFragment).setInterval(mPosStart, mPosEnd);
+    }
+
+    private void moveStepForward() {
+        if (mIsStartActive) {
+            mPosStart++;
+
+            if (mPosStart > mPosEnd) {
+                mPosEnd = mPosStart;
+            }
+        } else {
+            mPosEnd++;
+        }
+
+        ((MainFragment)mCurrentFragment).setInterval(mPosStart, mPosEnd);
+    }
+
+    private void moveStepBackward() {
+        if (mIsStartActive) {
+            mPosStart--;
+        } else {
+            mPosEnd--;
+
+            if (mPosEnd < mPosStart) {
+                mPosStart = mPosEnd;
+            }
+        }
+
+        ((MainFragment)mCurrentFragment).setInterval(mPosStart, mPosEnd);
+    }
+
+    @Override
+    public void OnMoveStepForward() {
+        moveStepForward();
+    }
+
+    @Override
+    public void OnMoveForward() {
+        moveForward();
+    }
+
+    @Override
+    public void OnMoveStepBackward() {
+        moveStepBackward();
+    }
+
+    @Override
+    public void OnMoveBackward() {
+        moveBackward();
+    }
+
+    @Override
+    public void OnSetPosition() {
+        setPosition();
+    }
+
+    @Override
+    public void OnConfigure() {
+        configure();
+    }
+
+    @Override
+    public void OnStartEndSession() {
+        startEndSession();
+    }
+
+    @Override
+    public void OnTakePhoto() {
+        takePhoto();
+    }
+}
---
+++ app/src/main/java/com/jocajica/project_004/fragments/MainFragment.java
@@ -0,0 +1,208 @@
+package com.jocajica.project_004.fragments;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.jocajica.project_004.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment implements View.OnClickListener {
+
+    private Button mButtonStepBackward;
+    private Button mButtonStepForward;
+    private Button mButtonBackward;
+    private Button mButtonForward;
+    private Button mButtonStartEndPosition;
+    private Button mButtonSettings;
+    private Button mButtonRunStop;
+    private Button mButtonTakePhoto;
+
+    private TextView mTextViewDistance;
+    private TextView mTextViewSteps;
+    private TextView mTextViewTime;
+
+    private int mPosStart;
+    private int mPosEnd;
+
+    private OnMainListener mCallback;
+
+    public MainFragment() {
+        mPosStart = 0;
+        mPosEnd = 0;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            mCallback = (OnMainListener) context;
+        } catch (Exception e) {
+            try {
+                throw new Exception(e.getMessage());
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallback = null;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        initUI(v);
+        updateInfo();
+
+        return v;
+    }
+
+    private void initUI(View v) {
+        mButtonStepBackward = v.findViewById(R.id.buttonStepBackward);
+        mButtonStepForward = v.findViewById(R.id.buttonStepForward);
+        mButtonBackward = v.findViewById(R.id.buttonBackward);
+        mButtonForward = v.findViewById(R.id.buttonForward);
+        mButtonStartEndPosition = v.findViewById(R.id.buttonStartEndPosition);
+        mButtonSettings = v.findViewById(R.id.buttonSettings);
+        mButtonRunStop = v.findViewById(R.id.buttonRunStop);
+        mButtonTakePhoto = v.findViewById(R.id.buttonTakePhoto);
+
+        mButtonStepBackward.setOnClickListener(this);
+        mButtonStepForward.setOnClickListener(this);
+        mButtonBackward.setOnClickListener(this);
+        mButtonForward.setOnClickListener(this);
+        mButtonStartEndPosition.setOnClickListener(this);
+        mButtonSettings.setOnClickListener(this);
+        mButtonRunStop.setOnClickListener(this);
+        mButtonTakePhoto.setOnClickListener(this);
+
+        mTextViewDistance = v.findViewById(R.id.textViewDistance);
+        mTextViewSteps = v.findViewById(R.id.textViewSteps);
+        mTextViewTime = v.findViewById(R.id.textViewTime);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonStepBackward:
+                moveStepBackward();
+                break;
+            case R.id.buttonStepForward:
+                moveStepForward();
+                break;
+            case R.id.buttonBackward:
+                moveBackward();
+                break;
+            case R.id.buttonForward:
+                moveForward();
+                break;
+            case R.id.buttonStartEndPosition:
+                setPosition();
+                break;
+            case R.id.buttonSettings:
+                configure();
+                break;
+            case R.id.buttonRunStop:
+                startEndSession();
+                break;
+            case R.id.buttonTakePhoto:
+                takePhoto();
+                break;
+        }
+    }
+
+    private void takePhoto() {
+        mCallback.OnTakePhoto();
+    }
+
+    private void startEndSession() {
+        mCallback.OnStartEndSession();
+    }
+
+    private void configure() {
+        mCallback.OnConfigure();
+    }
+
+    private void setPosition() {
+        mCallback.OnSetPosition();
+    }
+
+    private void moveForward() {
+        mCallback.OnMoveForward();
+    }
+
+    private void moveBackward() {
+        mCallback.OnMoveBackward();
+    }
+
+    private void moveStepForward() {
+        mCallback.OnMoveStepForward();
+    }
+
+    private void moveStepBackward() {
+        mCallback.OnMoveStepBackward();
+    }
+
+    public void setInterval(int posStart, int posEnd) {
+        mPosStart = posStart;
+        mPosEnd = posEnd;
+        updateInfo();
+    }
+
+    public void setStartEndStatus(boolean status) {
+        if (status) {
+            mButtonRunStop.setText(R.string.parar);
+        } else {
+            mButtonRunStop.setText(R.string.ejecutar);
+        }
+    }
+
+    public void setRunStopStatus(boolean status) {
+        if (status) {
+            mButtonStartEndPosition.setText(R.string.posicion_inicial);
+        } else {
+            mButtonStartEndPosition.setText(R.string.posicion_final);
+        }
+    }
+
+    public interface OnMainListener {
+        void OnMoveStepForward();
+        void OnMoveForward();
+        void OnMoveStepBackward();
+        void OnMoveBackward();
+        void OnSetPosition();
+        void OnConfigure();
+        void OnStartEndSession();
+        void OnTakePhoto();
+    }
+
+    private void updateInfo() {
+        Resources res = getResources();
+
+        String strDistance = String.format(res.getString(R.string.mostrar_distancia), (mPosEnd - mPosStart) * 0.4);
+        String strSteps = String.format(res.getString(R.string.mostrar_pasos), mPosEnd - mPosStart);
+        String strTime = String.format(res.getString(R.string.mostrar_tiempo), (mPosEnd - mPosStart) * 2);
+
+        mTextViewDistance.setText(strDistance);
+        mTextViewSteps.setText(strSteps);
+        mTextViewTime.setText(strTime);
+    }
+
+}
