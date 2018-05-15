08d18cc00be348911f11d8cf1d4a81d2a4ae25e9
jcarles <jcarles@kitchenmaster.es>
2018-05-15 13:07:28 +0200

    Creada la pantalla splash.

--- app/src/main/java/com/jocajica/project_004/activities/SplashActivity.java
+++ app/src/main/java/com/jocajica/project_004/activities/SplashActivity.java
@@ -1,15 +1,15 @@
 package com.jocajica.project_004.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.jocajica.project_004.R;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
     }
 }
